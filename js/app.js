6961aa058b0790a53c7f69e3d7b4e153b187becf
lucasi420 <lucasismaelf@yahoo.com>
2025-12-19 18:06:10 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -121,7 +121,7 @@ async function finalizar() {
     document.getElementById("resumenFinal").innerHTML = `<p style="color: #0b3c5d;">⌛ Generando PDF y enviando a Drive...</p>`;
 
     // 3. URL DE TU SCRIPT
-    const urlScript = "https://script.google.com/macros/s/AKfycbwUNqKrh3Fua2xypnfH_B-bAoRrRfGUyLmGK-1w68Dsd7gjxQMOHfb-hvnjXraziCm_LQ/exec"; 
+    const urlScript = "https://script.google.com/macros/s/AKfycbwGXRE3YvHd6Zi_02U1QCWHzGrZ04BG6472obifM4yoiUSTtVjKhO6uqntUtPJeFkL7kw/exec"; 
 
     const payload = {
         imagen: firmaData.split(',')[1], 
